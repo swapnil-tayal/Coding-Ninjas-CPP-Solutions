afc8fe7cd826dcf6b8314a4f38d1a1195395496c
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2022-04-06 10:00:19 +0530

    Update OperatorsAndForLoop.cpp

--- OperatorsAndForLoop.cpp
+++ OperatorsAndForLoop.cpp
@@ -59,3 +59,142 @@ int main(){
 }
 
 
+
+// Count Characters
+
+#include<iostream>
+using namespace std;
+#include  <bits/stdc++.h>
+
+int main(){
+ 
+    char c;
+    cin>>c;
+    int small = 0;
+    int num = 0;
+    int left = 0;
+    
+    while (c != '$'){
+        if (c >= 'a' && c <= 'z'){
+            small++;
+        }else if (c >= '0' && c <= '9'){
+            num++;
+        }else{
+            left++;
+        }cin.get(c);
+    }cout<<small<<" "<<num<<" "<<left;
+  
+}
+
+
+// Sum or Product
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+    
+    int n;
+    cin>>n;
+    
+    int c;
+    cin>>c;
+    
+    if (c == 1){
+        int sum = 0;
+        for (int i=1; i<=n; i++){
+            sum += i;
+        }cout<<sum;
+    }else if(c == 2){
+        int p = 1;
+        for (int i=2; i<=n; i++){
+            p *= i;
+        }cout<<p;
+    }else{
+        cout<<-1;
+    }
+	
+}
+
+
+
+// Terms of AP
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+	
+    int n;
+    cin>>n;
+    
+    int i=1;
+    int j=1;
+    
+    while (i <= n){
+        int s = 3*i + 2;
+        
+        if (s%4 != 0){
+            cout<<s<<" ";
+        }else{
+            n++;
+        }i++;
+        
+    }
+    
+}
+
+
+// Reverse of a number
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+    
+    int n;
+    cin>>n;
+    int rev = 0;
+    int digit;
+    
+    while (n > 0){
+        digit = n%10;
+        
+        rev = rev*10 + digit;
+        n = n/10;
+    }cout<<rev;
+	
+}
+
+
+
+// Binary to decimal
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+    
+    int n;
+    cin>>n;
+    int digit = 0;
+    int value;
+    int base = 1;
+    
+    while (n > 0){
+        
+        digit = n%10;
+        value += digit * base;
+        base = 2*base;
+        n = n/10;
+        
+    }cout<<value;
+	
+}
+
